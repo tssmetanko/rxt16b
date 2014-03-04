ca3a1bafec67938f80c903dad52d91bd28ec30a5
Timophey Smetanko <tssmetanko@gmail.com>
2014-03-04 23:33:40 +0400

    Changes in set_ifcfg.sh

--- set_ifcfg.sh
+++ set_ifcfg.sh
@@ -2,10 +2,18 @@
 IP=$2
 IFNAME=$1
 
-[[ -z $IFNAME ]] && ( echo "Error, IFNAME is empty"; exit 255)
-[[ -z $IP ]]&&( echo "Error, IP is empty"; exit 255)
+#[[ -z $IFNAME ]] && ( echo "Error, IFNAME is empty"; exit 255)
+#[[ -z $IP ]]&&( echo "Error, IP is empty"; exit 255)
+#HWADDR=$( ifconfig ${IFNAME} | grep 'HWaddr'| grep -oP '(\w{2}\:){5}\w{2}' )
+die(){
+	echo $1
+	exit 255
+}
 
-HWADDR=$( ifconfig ${IFNAME} | grep 'HWaddr'| grep -oP '(\w{2}\:){5}\w{2}' )
+get_hwaddr_by_ifname(){
+	local ifname=$1
+	ifconfig ${ifname} | grep 'HWaddr'| grep -oP '(\w{2}\:){5}\w{2}'
+}
 
 template(){
 cat <<TEMP1
@@ -20,4 +28,8 @@ TYPE="Ethernet"
 TEMP1
 }
 
-template > /etc/sysconfig/network-scripts/ifcfg-${IFNAME}
+[[ -z $IFNAME ]] && die "Error, IFNAME is empty"
+[[ -z $IP ]]&& die "Error, IP is empty"
+
+HWADDR=$(get_hwaddr_by_ifname ${IFNAME})
+template > /etc/sysconfig/network-scripts/ifcfg-${IFNAME} && echo "ifcfg-${IFNAME} writen" || die "Can not complete operation"
